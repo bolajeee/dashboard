c30583654e303f9ceafda2562bde4fa642537f97
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-08-10 16:36:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
-import { BrowserRouter, Form, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-
-import {Navbar, Footer, Sidebar, ThemeSettings, Stacked} from "./components"
-import { Customers, Kanban, Area, Bar, Pie, Calender, Financial, ColorPicker, ColorMapping, Line, Editor, Pyramid, Employees, Orders, Ecommerce } 
-from './pages'
+import { Navbar, Footer, Sidebar, ThemeSetting } from "./components";
+import {
+  Customers,
+  Kanban,
+  Area,
+  Stacked,
+  Bar,
+  Pie,
+  Pyramid,
+  Calender,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Line,
+  Editor,
+  Employees,
+  Orders,
+  Ecommerce,
+} from "./pages";
 
 import "./App.css";
 
 const App = () => {
-  const activeMenu = false;
- 
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
         <div>
           <div className="flex relative dark:bg-main-dark-bg">
-            <div className="fixed right-4 bottom-4 " style={{ zindex: "1000" }}>
+            <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
               <TooltipComponent content="settings" position="Top">
                 <button
                   type="button"
@@ -37,34 +52,33 @@ const App = () => {
             {activeMenu ? (
               <div
                 className="w-72 fixed sidebar dark:bg-secondary-dark-bg
-              bg-white 
-              "
+              bg-white"
               >
                 <Sidebar />
               </div>
             ) : (
-                <div className="w-0 dark:bg-secondary-dark-bg">
-                  {Sidebar
-                  }</div>
+              <div className="w-0 dark:bg-secondary-dark-bg">
+                <Sidebar />
+              </div>
             )}
             <div
               className={`dark:bg-main-bg bg-main-bg min-h-screen w-full
-               ${activeMenu ? " md:ml-72" : "flex-2"}
+               ${activeMenu ? "md:ml-72" : "flex-2"}
               `}
             >
               <div
                 className="fixed md:static bg-main-bg
               dark:bg-main-dark-bg navbar w-full"
               >
-                <navbar />
+                <Navbar />
               </div>
             </div>
 
             <div>
               <Routes>
-                {/* DashBoard */}
-                <Route path="/" element={<Ecommerce />}></Route>
-                <Route path="/ecommerce" element={<Ecommerce />}></Route>
+                {/* Dashboard */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
 
                 {/* Pages */}
                 <Route path="/orders" element={<Orders />} />
@@ -72,17 +86,17 @@ const App = () => {
                 <Route path="/customers" element={<Customers />} />
 
                 {/* Apps */}
-                <Route path="/Kanban" element={<Kanban />} />
-                <Route path="/editor" element={<Editor/>} />
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
                 <Route path="/calender" element={<Calender />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
-                {/* charts */}
+                {/* Charts */}
                 <Route path="/line" element={<Line />} />
                 <Route path="/area" element={<Area />} />
                 <Route path="/bar" element={<Bar />} />
                 <Route path="/pie" element={<Pie />} />
-                <Route path="/financial" element="Financial" />
+                <Route path="/financial" element={<Financial />} />
                 <Route path="/color-mapping" element={<ColorMapping />} />
                 <Route path="/pyramid" element={<Pyramid />} />
                 <Route path="/stacked" element={<Stacked />} />
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
+import "./index.css";
+import App from "./App";
 
-import './index.css'
-import App from './App'
-
-ReactDOM.render(
-  <>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
